aca6cebcc8d18189d4c33af9b663390d0febb81f
Marufibragimov11 <ibragimovmaruf1108@gmail.com>
2023-11-14 09:20:28 -0500

    solved 3rd problem

--- problems.py
+++ problems.py
@@ -1,5 +1,5 @@
 """
-                        Two Sum
+                                Two Sum
 Given an array of integers nums and an integer target,
 return indices of the two numbers such that they add up to target.
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
@@ -71,23 +71,22 @@ Explanation: L = 50, V= 5, III = 3.
 """
 
 
-def roman_to_int(x):
-    roman_nums = {
-        "I": 1,
-        "V": 5,
-        "X": 10,
-        "L": 50,
-        "C": 100,
-        "D": 500,
-        "M": 1000
-    }
+def roman_to_int(s):
+    roman_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    result = 0
+    prev_value = 0
 
-    ans = 0
-
-    for i in range(len(x)):
-        if i < len(x) - 1 and roman_nums[x[i]] < roman_nums[x[i + 1]]:
-            ans -= roman_nums[x[i]]
+    for c in s:
+        current_value = roman_values[c]
+        if current_value > prev_value:
+            result += current_value - 2 * prev_value if prev_value != 0 else current_value
         else:
-            ans += roman_nums[x[i]]
+            result += current_value
+        prev_value = current_value
 
-    return ans
+    return result
+
+
+roman_numeral = "XXVII"
+print(roman_to_int(roman_numeral))
+
