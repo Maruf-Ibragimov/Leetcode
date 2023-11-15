8c1cc3dfed62085976a97d097aade08fa6afb7e1
Marufibragimov11 <ibragimovmaruf1108@gmail.com>
2023-11-15 12:38:27 -0500

    solved 5th problem

--- longest_common_prefix.py
+++ longest_common_prefix.py
@@ -12,7 +12,7 @@ Example 2:
 
 Input: strs = ["dog","racecar","car"]
 Output: ""
-Explanation: There is no common prefix among the input strings.
+Explanation: There is no common prefix amongst the input strings.
 """
 
 
